b7d3a8c2a03787147a5afcfa1cd9bd18f7c67349
Alejandra Fernandez <alejajaf.g@mail.com>
2021-07-29 21:18:08 -0500

    Creando la clase padre y clases hijas(Polimorfismo y herencia)

---
+++ src/main/java/com/misiontic/project05_uml/Polygon.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.misiontic.project05_uml;
+
+/**
+ *
+ * @author aleja
+ */
+public class Polygon {
+    private double area;
+    private double perimeter;
+
+   
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+
+    public void setPerimeter(double perimeter) {
+        this.perimeter = perimeter;
+    }
+}
